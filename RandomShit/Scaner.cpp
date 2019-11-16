30cc7bd179ab90de6ca96135680b640dc334e563
d3mage <d3magexgod@gmail.com>
2019-11-16 22:57:34 +0200

    Update Scaner.cpp

--- RandomShit/Scaner.cpp
+++ RandomShit/Scaner.cpp
@@ -18,18 +18,18 @@ bool isBeginningCorrect(string& line)
 	else return false;
 }
 
-bool isSecondSymbolCorrect(string& line) 
+bool isSecondSymbolCorrect(string& line)
 {
 	if (line[1] == '+' || line[1] == '-')
 		return true;
-	return false;	
+	return false;
 }
 
- bool isSequenceCorrect(string& line, int &state)
+bool isSequenceCorrect(string& line, int& state)
 {
 	char alphabet[]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' };
 	char numbers[]{ 48, 49, 50, 51, 52, 53 };
-	for (int i = 0; i<11; i++)
+	for (int i = 0; i < 11; i++)
 	{
 		if (line[2] == alphabet[i])
 		{
@@ -37,7 +37,7 @@ bool isSecondSymbolCorrect(string& line)
 			return true;
 		}
 	}
-	for (int i = 0; i < 6; i++) 
+	for (int i = 0; i < 6; i++)
 	{
 		if (line[2] == numbers[i])
 		{
@@ -47,60 +47,60 @@ bool isSecondSymbolCorrect(string& line)
 	}
 }
 
- bool isCompletelyCorrectNumbers(string& line, char numbers[]) {
-	 for (int i = 3; i < line.length(); i++) {
-		 for (int j = 0; j < 6; j++)
-		 {
-			 if (line[i] == numbers[j])
-			 {
-				 break;
-			 }
-			 else if (j == 5)
-				 return false;
-		 }
-	 }
-	 return true;
- }
+bool isCompletelyCorrectNumbers(string& line, char numbers[]) {
+	for (int i = 3; i < line.length(); i++) {
+		for (int j = 0; j < 6; j++)
+		{
+			if (line[i] == numbers[j])
+			{
+				break;
+			}
+			else if (j == 5)
+				return false;
+		}
+	}
+	return true;
+}
 
- bool isCompletelyCorrectLetters(string& line, char letters[]) {
-	 for (int i = 3; i < line.length(); i++) {
-		 for (int j = 0; j < 11; j++)
-		 {
-			 if (line[i] == letters[j])
-			 {
-				 break;
-			 }
-			 else if (j == 10)
-				 return false;
-		 }
-	 }	
-	 return true;
- }
+bool isCompletelyCorrectLetters(string& line, char letters[]) {
+	for (int i = 3; i < line.length(); i++) {
+		for (int j = 0; j < 11; j++)
+		{
+			if (line[i] == letters[j])
+			{
+				break;
+			}
+			else if (j == 10)
+				return false;
+		}
+	}
+	return true;
+}
 
- int scannerOfLine()
- {
-	 char alphabet[]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' };
-	 char numbers[]{ 48, 49, 50, 51, 52, 53 };
-	 int state;
-	 string line = getLine();
-	 if (   isBeginningCorrect(line) != true
-		 || isSecondSymbolCorrect(line) != true
-		 || isSequenceCorrect(line, state) != true)
-		 return -1;
-      if (state == 0)
-			return isCompletelyCorrectLetters(line, alphabet);
-	  if (state == 1)
-		  return isCompletelyCorrectNumbers(line, numbers);
-	  else return -1;
- }
+int scannerOfLine()
+{
+	char alphabet[]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' };
+	char numbers[]{ 48, 49, 50, 51, 52, 53 };
+	int state;
+	string line = getLine();
+	if (isBeginningCorrect(line) != true
+		|| isSecondSymbolCorrect(line) != true
+		|| isSequenceCorrect(line, state) != true)
+		return -1;
+	if (state == 0)
+		return isCompletelyCorrectLetters(line, alphabet);
+	if (state == 1)
+		return isCompletelyCorrectNumbers(line, numbers);
+	else return -1;
+}
 
- void printAnswer()
- {
-	 int result = scannerOfLine();
-	 if (result == 1)
-		 cout << "This string is OK";
-	 else cout << "String is mistaken";
- }
+void printAnswer()
+{
+	int result = scannerOfLine();
+	if (result == 1)
+		cout << "This string is OK";
+	else cout << "String is mistaken";
+}
 
 void main()
 {
