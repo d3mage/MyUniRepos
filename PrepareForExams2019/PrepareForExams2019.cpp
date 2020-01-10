2e483cebf1c07911e9e63d12603295874281c513
d3mage <d3magexgod@gmail.com>
2020-01-11 00:47:28 +0200

    yes

--- PrepareForExams2019/PrepareForExams2019.cpp
+++ PrepareForExams2019/PrepareForExams2019.cpp
@@ -28,11 +28,12 @@ void prepareExam(string name, int subject)
 	bool back = false; 
 	while (back != true)
 	{
-		cout << "----------------------------------" << endl;
-		cout << "1. Рандомне питання" << endl;
-		cout << "2. Питання за номером" << endl;
-		cout << "3. Повернутися" << endl;
-		cout << "----------------------------------" << endl;
+		system("cls");
+		cout << "-------------------------------------" << endl;
+		cout << "| 1. Рандомне питання               |" << endl;
+		cout << "| 2. Питання за номером             |" << endl;
+		cout << "| 3. Повернутися                    |" << endl;
+		cout << "-------------------------------------" << endl;
 		int number; 
 		cin >> number;
 		cin.ignore();
@@ -58,6 +59,7 @@ void prepareExam(string name, int subject)
 			cin >> question;
 			cin.ignore();
 			cout << values[question-1]<<endl;
+			_getch();
 		}
 		else if (number == 3)
 		{
@@ -71,12 +73,12 @@ void menu()
 	bool exit = false; 
 	while (exit != true)
 	{
-		cout << "----------------------------------" << endl;
-		cout << "До чого ми готуємось?"<<endl;
-		cout << "1. ІПЗ"<<endl;
-		cout << "2. Історія" << endl;
-		cout << "Вихід" << endl;
-		cout << "----------------------------------" << endl;
+		cout << "-------------------------------------" << endl;
+		cout << "| До чого ми готуємось?            |"<<endl;
+		cout << "| 1. ІПЗ                           |"<<endl;
+		cout << "| 2. Історія                       |" << endl;
+		cout << "| Вихід                            |" << endl;
+		cout << "-------------------------------------" << endl;
 		int number;
 		cin >> number; 
 		cin.ignore();
